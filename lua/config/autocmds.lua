b65552c93d8f2dca6b83e2343ae93acc63c73a9e
Farrell, Gabriel <Gabriel.Farrell@nyulangone.org>
2024-10-17 00:01:17 -0400

    Add autocommands for cloudformation

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,37 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+print("Loading autocmds.lua")
+
+-- add autocmds here
+local function detect_cloudformation()
+  if vim.fn.search("AWSTemplateFormatVersion", "nw") > 0 then
+    vim.bo.filetype = "yaml.cloudformation"
+    print("Detected CloudFormation file")
+  end
+end
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.yml", "*.yaml" },
+  callback = detect_cloudformation,
+})
+
+-- Add this to your init.lua or any file that's sourced during startup
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "yaml.cloudformation",
+  callback = function()
+    vim.schedule(function()
+      local active_clients = vim.lsp.get_active_clients({ bufnr = 0 })
+      for _, client in pairs(active_clients) do
+        if client.name == "yamlls" then
+          vim.lsp.stop_client(client.id)
+          print("Stopped yaml-language-server")
+          break
+        end
+      end
+    end)
+  end,
+})
+
+print("Loaded autocmds.lua")
