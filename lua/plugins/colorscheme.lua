ef9aae4e2f14854e90ac8417bb3d2ed87a45f6ca
Farrell, Gabriel <Gabriel.Farrell@nyulangone.org>
2024-10-17 19:14:49 -0400

    Use toykonight colorscheme and set as transparent

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  -- Configure LazyVim to use tokyonight
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
