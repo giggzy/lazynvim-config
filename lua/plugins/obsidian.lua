f3603fd7bc2382d57cc15768f1db184f70bf1d24
Farrell, Gabriel <Gabriel.Farrell@nyulangone.org>
2024-11-02 03:12:42 -0400

    Obsidian: change logic for note_id creation so name make some sense

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -27,6 +27,25 @@ return {
         path = "~/vaults/work/",
       },
     },
+    note_id_func = function(title)
+      -- Grabed this from  the project README as the default is just the random looking id
+      --
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end,
+
     -- Specify the path to your template
     templates = {
       subdir = "templates",
